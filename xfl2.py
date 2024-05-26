47997fb17fb15be847c655e951040148501d6c0e
meowweao <ganeshfalak01@gmail.com>
2024-05-26 19:15:26 +0530

    xfl2 is done

---
+++ xfl2.py
@@ -0,0 +1,14 @@
+# Perform String Operations
+
+def reverse_string(sample_string):
+    return sample_string[::-1]
+
+def reverse_array(sample_array):
+    return sample_array[::-1]
+
+def concatenate_string(str_one, str_two):
+    return str_one + str_two
+
+print(reverse_string("summon me"))
+print(reverse_array([1, 2, 3, 4, 5]))
+print(concatenate_string("summon", "me"))
